ffea17215c461267f4b6a57429e14c6c41b62b0e
trisskmasarahh <trisukmasarahh@gmail.com>
2025-03-05 01:02:07 +0700

    praktikum 4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/level', [LevelController::class, 'index']);
