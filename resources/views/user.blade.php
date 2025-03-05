94a24d49867fc46dc826b5ac2ed65b237d65eec9
trisskmasarahh <trisukmasarahh@gmail.com>
2025-03-05 11:31:23 +0700

    2.4 pertemuan4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,19 +7,19 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
-            {{-- <th>ID</th>
+            {{-- <th>Jumlah Pengguna</th> --}}
+            <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th> --}}
+            <th>ID Level Pengguna</th>
         </tr>
         {{-- @foreach ($data as $d) --}}
         <tr>
-            <td>{{ $data}}</td>
-            {{-- <td>{{ $data->user_id }}</td>
+            {{-- <td>{{ $data}}</td> --}}
+            <td>{{ $data->user_id }}</td>
             <td>{{ $data->username }}</td>
             <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td> --}}
+            <td>{{ $data->level_id }}</td>
         </tr>
     
     </table>
