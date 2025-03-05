14a51b0924c4eb65edafbc25ee0a19bbe9f20a39
trisskmasarahh <trisukmasarahh@gmail.com>
2025-03-05 10:12:38 +0700

    2.2 pertemuan 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,15 +11,18 @@ class UserController extends Controller
     public function index()
     {
 
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
+
         // $user = UserModel::find(1);
         // $user = UserModel::firstWhere('level_id', 1)->first();
         // $user = UserModel::findOr(1, ['username', 'nama'],function (){
         //     abort(404);
         // });
-        $user = UserModel::findOr(20, ['username', 'nama'],function (){
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(20, ['username', 'nama'],function (){
+        //     abort(404);
+        // });
+        // $user = UserModel::findOrFail(1);
     }
 }
         // $data =[
