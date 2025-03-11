71af2b493213db03294ac1b501ce6a929c8b1842
trisskmasarahh <trisukmasarahh@gmail.com>
2025-03-11 13:01:57 +0700

    js4 praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,17 +13,45 @@ class UserController extends Controller
 
         // $user = UserModel::where('level_id', 2)->count();
         // dd($user);
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' =>'Manager Tiga Tiga',
+                // 'username' => 'manager55',
+                // 'nama' =>'Manager55',
+                // 'password' => Hash::make('12345'),
+                // 'level_id'=> 2 ,
+                'username' => 'manager11',
+                'nama' =>'Manager11',
                 'password' => Hash::make('12345'),
-                'level_id'=> 2
-            ]
-            );
+                'level_id'=> 2 ,
+            ]);
+            // $user->username= 'manager56';
+            $user->username= 'manager12';
+
+            // $user->isDirty();//true
+            // $user->isDirty('username');//true
+            // $user->isDirty('nama'); // false
+            // $user->isDirty(['nama', 'username']);//true
+
+            // $user->isClean(); // false
+            // $user->isClean('username'); // false
+            // $user->isClean('nama'); // true
+            // $user->isClean(['nama', 'username']);//false
+
             $user->save();
+            $user->wasChanged();
+            $user->wasChanged('username');
+            $user->wasChanged(['username', 'level_id']);
+            $user->wasChanged('nama');
+            dd($user->wasChanged(['nama', 'username']));
+
+            // $user->isDirty(); // false
+            // $user->isClean(); // true
+
+            // dd($user->isDirty());
+    }
+}
             
-        return view('user', ['data' => $user]);
+        // return view('user', ['data' => $user]);
 
         // $user = UserModel::find(1);
         // $user = UserModel::firstWhere('level_id', 1)->first();
@@ -34,8 +62,8 @@ class UserController extends Controller
         //     abort(404);
         // });
         // $user = UserModel::findOrFail(1);
-    }
-}
+    
+
         // $data =[
         //     'level_id' => 2,
         //     'username' => 'manager_tiga',
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,5 +11,5 @@ class UserModel extends Model
     protected $table ='m_user';
     protected $primaryKey='user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
