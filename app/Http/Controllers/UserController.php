1b5865092c2b9270f0220af8a0d84414090b4a7f
trisskmasarahh <trisukmasarahh@gmail.com>
2025-03-11 22:50:06 +0700

    js 4 praktikum 2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,47 +11,51 @@ class UserController extends Controller
 { 
     public function index()
     {
-        $user = UserModel::all();
-        return view('user', ['data'=> $user]);
-    }
-    public function tambah()
-    {
-        return view('user_tambah');
-    }
-    public function tambah_simpan(Request $request)
-    {
-        UserModel::create([
-            'username' => $request->username,
-            'nama' => $request->nama,
-            'password' => Hash::make('$request->password'),
-            'level_id'=> $request->level_id
-        ]);
-        return redirect('/user');
-    }
-    public function ubah($id)
-    {
-        $user = UserModel ::find($id);
-        return view('user_ubah', ['data' => $user]);
-    }
+        $user = UserModel::with('level')->get();
+        return view('user', ['data' => $user]);
 
-    public function ubah_simpan($id, Request $request)
-    {
-        $user = UserModel::find($id);
+        // $user = UserModel::with('level')->get();
+        // dd($user);
+        // return view('user', ['data'=> $user]);
+    // }
+    // public function tambah()
+    // {
+    //     return view('user_tambah');
+    // }
+    // public function tambah_simpan(Request $request)
+    // {
+    //     UserModel::create([
+    //         'username' => $request->username,
+    //         'nama' => $request->nama,
+    //         'password' => Hash::make('$request->password'),
+    //         'level_id'=> $request->level_id
+    //     ]);
+    //     return redirect('/user');
+    // }
+    // public function ubah($id)
+    // {
+    //     $user = UserModel ::find($id);
+    //     return view('user_ubah', ['data' => $user]);
+    // }
 
-        $user->username = $request->username;
-        $user->nama= $request->nama;
-        $user->password= Hash::make('$request->password');
-        $user->Level_id = $request->level_id;
+    // public function ubah_simpan($id, Request $request)
+    // {
+    //     $user = UserModel::find($id);
 
-        $user->save();
-        return redirect('/user');
-    }
-    public function hapus($id)
-    {
-        $user = UserModel::find($id);
-        $user->delete();
+    //     $user->username = $request->username;
+    //     $user->nama= $request->nama;
+    //     $user->password= Hash::make('$request->password');
+    //     $user->Level_id = $request->level_id;
 
-        return redirect('/user');
+    //     $user->save();
+    //     return redirect('/user');
+    // }
+    // public function hapus($id)
+    // {
+    //     $user = UserModel::find($id);
+    //     $user->delete();
+
+    //     return redirect('/user');
     }
 }
     
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,6 +10,8 @@
             <td>Username</td>
             <td>Nama</td>
             <td>ID Level Pengguna</td>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
             <td>Aksi</td>
         </tr>
     
@@ -19,6 +21,8 @@
             <td>{{ $d->username }}</td>
             <td>{{ $d->nama }}</td>
             <td>{{ $d->level_id }}</td>
+            <td>{{$d->level->level_kode}}</td>
+            <td>{{$d->level->level_nama}}</td>
             <td>
             <a href="/user/ubah/{{ $d->user_id }}">Ubah</a> |
             <a href="/user/hapus/{{ $d->user_id }}">Hapus</a>
