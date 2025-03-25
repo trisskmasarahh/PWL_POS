a99292601aca8cdbbcd261e8a50c3db03255ffe3
trisskmasarahh <trisukmasarahh@gmail.com>
2025-03-25 19:58:41 +0700

    js 6 praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -57,9 +57,11 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/edit', [UserController::class, 'edit']);   // menampilkan halaman form edit user
 
     Route::put('/{id}', [UserController::class, 'update']);      // menyimpan perubahan data user
-    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);// pengeditan menggunkan ajax
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
 
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);//penghapusan menggunakan ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
     Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
 
 });
